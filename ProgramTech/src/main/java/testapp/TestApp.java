1445eb401343a19e86e66c153eb1bcc14e3b874d
Kamaletdinov Anatolii <anatolii.kamaletdinov@trackimo.com>
2020-09-16 00:33:53 +0300

    Git test application with scanner

--- ProgramTech/src/main/java/testapp/TestApp.java
+++ ProgramTech/src/main/java/testapp/TestApp.java
@@ -1,11 +1,16 @@
 package testapp;
 
+import java.util.Scanner;
+
 public class TestApp {
     public static void main(String[] args) {
         //TODO code application here
-        float a = 10;
-        float b = (float) 12.5;
+        Scanner in = new Scanner(System.in);
+        System.out.println("1st num: ");
+        double a = Double.valueOf(in.nextLine());
+        System.out.println("2nd num: ");
+        double b = Double.valueOf(in.nextLine());
         System.out.println("a+b="+(a+b));
-        System.out.println("GIT test Application");
+        System.out.println("GIT test application");
     }
 }
