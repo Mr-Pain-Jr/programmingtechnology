e35a8944ac8ed058d8dc1b3f7a8031c4909cb5aa
Kamaletdinov Anatolii <anatolii.kamaletdinov@trackimo.com>
2020-09-16 00:30:59 +0300

    GIT test Application

--- ProgramTech/src/main/java/testapp/TestApp.java
+++ ProgramTech/src/main/java/testapp/TestApp.java
@@ -3,6 +3,9 @@ package testapp;
 public class TestApp {
     public static void main(String[] args) {
         //TODO code application here
-        System.out.println("GIT test Kamaletdinov application");
+        float a = 10;
+        float b = (float) 12.5;
+        System.out.println("a+b="+(a+b));
+        System.out.println("GIT test Application");
     }
 }
