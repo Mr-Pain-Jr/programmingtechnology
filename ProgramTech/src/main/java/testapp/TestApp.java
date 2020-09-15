cde16e52b0c1f8f1793886961b607c97bda38a95
Kamaletdinov Anatolii <anatolii.kamaletdinov@trackimo.com>
2020-09-16 00:25:27 +0300

    Added testApp class

---
+++ ProgramTech/src/main/java/testapp/TestApp.java
@@ -0,0 +1,8 @@
+package testapp;
+
+public class TestApp {
+    public static void main(String[] args) {
+        //TODO code application here
+        System.out.println("GIT test Kamaletdinov application");
+    }
+}
